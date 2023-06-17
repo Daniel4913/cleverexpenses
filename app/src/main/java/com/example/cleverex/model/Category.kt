f82dfb60a7d8746451a777296a695f74200d1688
Daniel Sosnowski <danielsosnowski@MacBook-Air-Daniel.local>
2023-06-17 09:22:36 +0200

    value classes for Category

--- app/src/main/java/com/example/cleverex/model/Category.kt
+++ app/src/main/java/com/example/cleverex/model/Category.kt
@@ -3,9 +3,23 @@ package com.example.cleverex.model
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 
+
 class Category(
     val name: String = "",
     val icon: ImageVector,
     val color: Color,
     //todo date range - kiedy chce przefiltrowac kategorie z konkretnej
     )
+
+@JvmInline
+value class CategoryName(
+    val value: String
+)
+@JvmInline
+value class CategoryIcon(
+    val value: ImageVector
+)
+@JvmInline
+value class CategoryColor(
+    val value: Color
+)
